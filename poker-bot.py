5ec4ea44a9fa438a702babd5165031619f1267d6
cranberrymuffin <aparna_natarajan@brown.edu>
2021-01-23 13:37:11 -0500

    gitignore, dotenv set up for token

--- poker-bot.py
+++ poker-bot.py
@@ -1,9 +1,13 @@
 #import libraries
 import discord
 from discord.ext.commands import Bot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #Your bot token
-TOKEN="ask"
+TOKEN=os.getenv("TOKEN")
 
 #create bot object
 client = Bot(command_prefix="!")
@@ -14,6 +18,7 @@ async def on_message(message):
     print(message.author.id)
     print(message.content)
     if message.author.id != 802584001801748532:
-        await message.channel.send("i recieved a message!")
+        if "!start" in message.content.lower():
+            await message.channel.send("start game invoked!")
 
 client.run(TOKEN)
